3689fef8f7a60432a05a016dcd5962826f03b1cf
fahmikhairudin <fahmikhoirudin135@gmail.com>
2021-10-28 22:22:10 +0700

    Progress MiniProject Mobile

---
+++ app/src/main/java/org/aplas/a1941720151_fahmikhairudin/MainActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.a1941720151_fahmikhairudin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
